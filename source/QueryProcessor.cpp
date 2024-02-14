e2a4e12bb0c9f7abed00bca832b41bebf7b72c92
e0493677 <e0493677@u.nus.edu>
2024-02-15 00:09:49 +0800

    Added condition to handle all synonym to query the select statement.

--- source/QueryProcessor.cpp
+++ source/QueryProcessor.cpp
@@ -25,17 +25,18 @@ void QueryProcessor::evaluate(string query, vector<string>& output) {
 	// check what type of synonym is being declared
 
 	map<string, string> synonymMap; //will store (p, procedure)
+	string varName;
+	string synonymType;
 
 	for (size_t i = 0; i < tokens.size(); ++i) {
 		const string& token = tokens[i];
 		if (token == ";") {
-			string varName = tokens[i - 2];
-			string syn = tokens[i - 1];
-			synonymMap[syn] = varName;
+			varName = tokens[i - 1];
+			synonymType = tokens[i - 2];
+			synonymMap[synonymType] = varName;
+			break;
 		}
 	}
-
-	string synonymType = tokens.at(0);
 
 	// create a vector for storing the results from database
 	vector<string> databaseResults;
@@ -43,8 +44,32 @@ void QueryProcessor::evaluate(string query, vector<string>& output) {
 	// call the method in database to retrieve the results
 	// This logic is highly simplified based on iteration 1 requirements and 
 	// the assumption that the queries are valid.
-	if (synonymType == "procedure") {
-		Database::getProcedures(databaseResults);
+	
+	if (!synonymType.empty()) {
+		string tableName = "";
+
+		if (synonymType == "procedure"){
+			tableName = "procedures";
+		}
+		if (synonymType == "variable") {
+			tableName = "variables";
+		}
+		if (synonymType == "constant") {
+			tableName = "constants";
+		}
+		if (synonymType == "assign") {
+			tableName = "assignments";
+		}
+		if (synonymType == "print") {
+			tableName = "prints";
+		}
+		if (synonymType == "read") {
+			tableName = "reads";
+		}
+		if (synonymType == "stmt") {
+			tableName = "statements";
+		}
+		Database::getData(tableName,databaseResults);
 	}
 
 	// post process the results to fill in the output vector
